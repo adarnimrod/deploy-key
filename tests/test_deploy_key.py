9fd2ad8d82606a6e497e01f9e1736549d29d2052
Adar Nimrod <nimrod@shore.co.il>
2016-11-21 08:18:20 +0200

    - Added mail alias.

--- tests/test_deploy_key.py
+++ tests/test_deploy_key.py
@@ -1,3 +1,12 @@
 def test_deploy_key(Command, Sudo):
     with Sudo(user='nobody'):
         assert Command('git -C /tmp/ansible-role-deploy-key pull').rc == 0
+
+
+def test_deply_key_alias(File, Ansible, User):
+    assert User('nobody').exists
+    ansible_os_family = Ansible('setup')['ansible_facts']['ansible_os_family']
+    if ansible_os_family == 'Debian':
+        assert File('/etc/aliases').contains('nobody: root')
+    elif ansible_os_family == 'OpenBSD':
+        assert File('/etc/mail/aliases').contains('nobody: root')
