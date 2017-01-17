b3c2aa4d2bfe880f711d3ec238ebfcebca7586e2
Adar Nimrod <nimrod@shore.co.il>
2017-01-17 13:43:58 +0200

    - Allow multiple users to use the deploy key.

--- tests/test_deploy_key.py
+++ tests/test_deploy_key.py
@@ -6,11 +6,3 @@ testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 def test_deploy_key(Command, Sudo):
     with Sudo(user='nobody'):
         assert Command('git -C /tmp/ansible-role-deploy-key pull').rc == 0
-
-
-def test_deply_key_alias(File, User, SystemInfo):
-    assert User('nobody').exists
-    if SystemInfo.type == 'linux':
-        assert File('/etc/aliases').contains('nobody: root')
-    elif SystemInfo.type == 'openbsd':
-        assert File('/etc/mail/aliases').contains('nobody: root')
